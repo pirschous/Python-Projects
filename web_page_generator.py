372e37d659099d071f2ba446ad410f81998bcf2a
pirschous <110510027+pirschous@users.noreply.github.com>
2023-04-23 20:25:46 -0700

    Add files via upload

---
+++ web_page_generator.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import *
+import webbrowser
+
+class ParentWindow(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master.title("Web Page Generator")
+        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
+        self.btn.grid(row=2, column=3, padx=(10,10), pady=(10,10))
+        self.btn = Button(self.master, text="Submit Custom Text", width=30, height=2, command=self.customHTML)
+        self.btn.grid(row=2, column=4, padx=(10,10), pady=(10,10))
+
+        self.source_dir = Entry(width=75)
+        #Positions entry in GUI using tkinter grid() padx and pady are the same as
+        #the button to ensure they will line up
+        self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))
+
+        
+
+    def defaultHTML(self):
+        htmlText = "Stay tuned for our amazing summer sale!"
+        htmlFile = open("index.html", "w")
+        htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
+        htmlFile.write(htmlContent)
+        htmlFile.close()
+        webbrowser.open_new_tab("index.html")
+
+    def customHTML(self):
+        customText = self.source_dir.get()
+        htmlFile = open("index.html", "w")
+        htmlContent = "<html>\n<body>\n<h1>" + customText + "</h1>\n</body>\n</html>"
+        htmlFile.write(htmlContent)
+        htmlFile.close()
+        webbrowser.open_new_tab("index.html")
+
+
+if __name__ + "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
